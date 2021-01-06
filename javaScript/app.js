d1aba0fd4af5c5e2102307f597be358bca5dc1b1
Harish-Prasad <hp.harishprasad@gmail.com>
2021-01-06 17:28:01 +0530

    notes display functionality

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,18 +1,52 @@
 
 
-let addBtn = document.getElementById('addBtn');
-addBtn.addEventListener("click",function(e){
+showNotes();
 
-    let addText = document.getElementById("addtxt");
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function (e) {
+
+    let addText = document.getElementById('addText');
     let notes = localStorage.getItem("notes");
-    if(notes == null){
+    if (notes == null) {
         notesObj = [];
     }
-    else{
+    else {
         notesObj = JSON.parse(notes);
     }
-    notes.push(addText.value);
-    localStorage.setItem("notes",JSON.stringify(notes));
+    notesObj.push(addText.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
     addText.value = "";
     console.log(notesObj);
+    showNotes();
 })
+
+
+function showNotes(){
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Note ${index + 1}</h5>
+                <p class="card-text">${element}</p>
+                <button class="btn btn-primary">Delete Note</button>
+            </div>
+        </div>  
+        `;
+
+    });
+    let notesElm = document.getElementById("notes");
+    if(notesObj.length != 0){
+        notesElm.innerHTML = html;
+    }
+    else{
+        notesElm.innerHTML = `No Notes in your Bag, Sorry!  :( `;
+    }
+}
